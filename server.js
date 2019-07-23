6a8da4dd603a8e5aed4c825ce50901e92fcdb045
sajafisher29 <sajafisher@gmail.com>
2019-07-23 12:14:04 -0700

    Browse search results functional

--- server.js
+++ server.js
@@ -11,8 +11,52 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 //API Routes
+
 //Test: http://localhost:3000/...
-app.get('/', (req, res) => {res.render('pages/index.ejs')});
+// app.get('/', (req, res) => {res.render('pages/index.ejs')});
+
+//Renders the search form
+app.get('/', newSearch);
+
+//Creates a new search to the Google Books API (Handler)
+app.post('/searches', createSearch);
+
+//Catch-all
+app.get('*', (request, response) => response.status(404).send('This route does not exist.'));
 
 //PORT listener
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
+//Helper functions
+function Book(info) {
+  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+
+  this.title = info.title;
+  this.authors = info.authors;
+  this.image = info.imageLinks.thumbnail;
+  this.description = info.description;
+}
+
+//Note that .ejs file extension is not required
+function newSearch(request, response) {
+  response.render('pages/index')
+}
+
+//Note that no API required
+
+//Console.log request.body and request.body.search
+function createSearch(request, response) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+  console.log(request.body);
+  console.log(request.body.search);
+
+  if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
+  if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
+
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => response.render('pages/searches/show', { searchResults: results }));
+}
+
+
